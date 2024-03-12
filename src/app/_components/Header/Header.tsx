4fff8d0857d742fb02bfd31c78fe6b3255b4d20d
Neha <leehanseok3@gmail.com>
2024-03-12 16:24:11 +0530

    styles: header

--- src/app/_components/Header/Header.tsx
+++ src/app/_components/Header/Header.tsx
@@ -1,8 +1,8 @@
 export const Header = () => {
   return (
-    <header className="flex justify-between px-8 py-6">
+    <header className="flex justify-between px-8 py-6 border-b border-slate-100">
       <a href="/"> Train Pro</a>
-      <div className="flex gap-4">
+      <div className="flex gap-8">
         <a href="/clients">Clients</a>
         <a href="/checkin">Check In</a>
         <a href="/library">Library</a>
